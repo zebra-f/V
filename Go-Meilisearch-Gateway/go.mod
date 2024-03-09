0c19bb010a83b5803a09241218e72700d4c07322
rafal <zebraf.dev@gmail.com>
2024-03-10 00:45:19 +0100

    feat: add meilisearch requirements, create draft of the search gateway

--- Go-Meilisearch-Gateway/go.mod
+++ Go-Meilisearch-Gateway/go.mod
@@ -1,3 +1,14 @@
 module github.com/zebra-f/V/Go-Meilisearch-Gateway
 
 go 1.21.4
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.6 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/meilisearch/meilisearch-go v0.26.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.37.1-0.20220607072126-8a320890c08d // indirect
+)
--- Go-Meilisearch-Gateway/main.go
+++ Go-Meilisearch-Gateway/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/meilisearch/meilisearch-go"
+	"net/http"
+	"os"
+)
+
+var meilisearchMasterKey = os.Getenv("MEILISEARCH_V_1_MASTER_KEY")
+var meilisearchUrl = os.Getenv("MEILISEARCH_URL")
+
+var client = meilisearch.NewClient(meilisearch.ClientConfig{
+	Host:   "http://127.0.0.1:7700",
+	APIKey: meilisearchMasterKey,
+})
+var index = client.Index("speeds")
 
 func main() {
-	fmt.Println("Placeholder")
+	address := "127.0.0.1:8080"
+
+	http.HandleFunc("/meilisearch/", meilisearchHandler)
+	server := &http.Server{
+		Addr:    address,
+		Handler: http.DefaultServeMux,
+	}
+
+	fmt.Printf("HTTP Server listening on %s...\n", address)
+
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
+
+func meilisearchHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("\n\n------")
+	queryParams := r.URL.Query()
+	q := queryParams.Get("q")
+	if q == "" {
+		err := fmt.Errorf("Missing 'q' parameter")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} else {
+		fmt.Println("q: ", q)
+	}
+	fmt.Println("query params:", queryParams)
+	fmt.Printf("request type: %T,\nrequest value: %v\n", r, r)
+	fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	searchRes, err := index.Search("test",
+		&meilisearch.SearchRequest{
+			Limit: 10,
+		})
+	fmt.Println("\n---------\nmeiliserach results:", searchRes)
+	fmt.Println("meiliearch err:", err)
 }
