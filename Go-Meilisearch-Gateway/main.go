7cdb1f96eba0dad37538b4192ad83730ff4574be
rafal <zebraf.dev@gmail.com>
2024-03-11 22:00:34 +0100

    feat: add/improve logging

--- Go-Meilisearch-Gateway/main.go
+++ Go-Meilisearch-Gateway/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/meilisearch/meilisearch-go"
+	"io"
+	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var meilisearchMasterKey = os.Getenv("MEILISEARCH_V_1_MASTER_KEY")
@@ -17,15 +20,27 @@ var client = meilisearch.NewClient(meilisearch.ClientConfig{
 var index = client.Index("speeds")
 
 func main() {
+	file, err := os.Create("logfile.log")
+	if err != nil {
+		log.Fatal("Error creating log file:", err)
+		panic(err)
+	}
+	defer file.Close()
+
+	multiWriter := io.MultiWriter(file, os.Stdout)
+	criticalLogger := log.New(multiWriter, "[CRITICAL] ", log.Ldate|log.Ltime)
+	errorLogger := log.New(multiWriter, "[ERROR] ", log.Ldate|log.Ltime)
+
 	address := "127.0.0.1:8080"
 
-	_, err := index.UpdateFilterableAttributes(&[]string{"is_public"})
+	_, err = index.UpdateFilterableAttributes(&[]string{"is_public"})
 	if err != nil {
 		fmt.Printf("Can't update filterable attributes in Meilisearch, %s\n", err)
+		criticalLogger.Printf("Can't update filterable attributes in Meilisearch, %s\n", err)
 		panic(err)
 	}
 
-	http.HandleFunc("/meilisearch/", meilisearchHandler)
+	http.HandleFunc("/meilisearch/", meilisearchHandler(errorLogger))
 	server := &http.Server{
 		Addr:    address,
 		Handler: http.DefaultServeMux,
@@ -33,30 +48,39 @@ func main() {
 
 	fmt.Printf("HTTP Server listening on %s...\n", address)
 	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("Error starting server: %s\n", err)
+		criticalLogger.Printf("Error starting server: %s\n", err)
 		panic(err)
 	}
 }
 
-func meilisearchHandler(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	q := queryParams.Get("q")
+func meilisearchHandler(errorLogger *log.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		queryParams := r.URL.Query()
+		q := queryParams.Get("q")
 
-	searchRes, err := index.Search(q,
-		&meilisearch.SearchRequest{
-			Limit:  10,
-			Filter: "is_public = true",
-		})
-	if err != nil {
-		http.Error(w, "Internal Server Search Error", http.StatusInternalServerError)
-		return
-	}
-	jsonSearchRes, err := searchRes.MarshalJSON()
-	if err != nil {
-		http.Error(w, "Internal Server JSON Error", http.StatusInternalServerError)
-		return
-	}
+		searchRes, err := index.Search(q,
+			&meilisearch.SearchRequest{
+				Limit:  10,
+				Filter: "is_public = true",
+			})
+		if err != nil {
+			errorLogger.Printf("Internal Server Search Error: %s\n", err)
+			http.Error(w, "Internal Server Search Error", http.StatusInternalServerError)
+			return
+		}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(jsonSearchRes)
+		jsonSearchRes, err := searchRes.MarshalJSON()
+		if err != nil {
+			errorLogger.Printf("Internal Server JSON Error: %s\n", err)
+			http.Error(w, "Internal Server JSON Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonSearchRes)
+
+		currentTime := time.Now()
+		fmt.Printf("[%s] Response sent successfully\n", currentTime.Format("2006-01-02 15:04:05"))
+	}
 }
